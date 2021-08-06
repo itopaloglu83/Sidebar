14855a3580b2e201ea3384dd950e69ab45866822
Ihsan TOPALOGLU <itopaloglu83@gmail.com>
2021-08-06 03:30:07 -0400

    Initial Commit

---
+++ Sidebar/SidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarApp.swift
+//  Sidebar
+//
+//  Created by İhsan TOPALOĞLU on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
