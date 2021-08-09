12b0437cdad5d5ad854af3346c067fd8eb5499c8
Ihsan TOPALOGLU <itopaloglu83@gmail.com>
2021-08-09 16:02:56 -0400

    Initial commit.

--- Sidebar/ContentView.swift
+++ Sidebar/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresenting = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            if !isPresenting {
+                Button("Open Sidebar") {
+                    isPresenting = true
+                }
+            }
+            
+            // Sidebar is on the top of ZStack but
+            // configured to be not visible.
+            SideMenu(width: 270, isOpen: $isPresenting)
+        }
+    }
+}
+
+struct SideMenu: View {
+    let width: CGFloat
+    
+    @Binding var isOpen: Bool
+    
+    var body: some View {
+        ZStack {
+            // Rectangle is normally fully transparent.
+            // When the sidebar is presented,
+            // its opacity is increased with animation.
+            Rectangle()
+                .foregroundColor(.clear)
+                .background(Color.gray.opacity(0.3))
+                .edgesIgnoringSafeArea(.all)
+                .opacity(isOpen ? 1 : 0)
+                .animation(Animation.easeIn.delay(0.25))
+                .onTapGesture {
+                    isOpen = false
+                }
+            
+            // Menu is normally pushed aside.
+            // When the sidebar is presented,
+            // it slides into the view.
+            HStack {
+                MenuContent()
+                    .frame(width: width)
+                    .offset(x: isOpen ? 0 : -width)
+                    .animation(.easeIn)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct MenuContent: View {
+    var body: some View {
+        List {
+            Text("My Profile")
+            Text("Posts")
+            Text("Logout")
+        }
     }
 }
 
